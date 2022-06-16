9ec55b64af5fd4f455e53770a54f758cd028bc66
Rahul Thakkar <rahul.thacker@radixweb.com>
2022-06-16 16:55:43 +0530

    Sequlize config changed

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,53 @@
+// module.exports = {
+//   HOST: process.env.MYSQL_HOST,
+//   USER: process.env.MYSQL_USER,
+//   DB: process.env.MYSQL_DATABASE,
+//   PASSWORD: process.env.MYSQL_PASSWORD,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
+const fs = require('fs');
+require('dotenv').config();
 module.exports = {
-  HOST: process.env.MYSQL_HOST,
-  USER: process.env.MYSQL_USER,
-  DB: process.env.MYSQL_DATABASE,
-  PASSWORD: process.env.MYSQL_PASSWORD,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
   },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true,
+    //   ssl: {
+    //     ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+    //   }
+    }
+  }
 };
