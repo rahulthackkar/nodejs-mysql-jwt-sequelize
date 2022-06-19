9bccb070be2ed7387dd7bb0a08d11f2946578ce2
Rahul Thakkar <rahul.thacker@radixweb.com>
2022-06-19 13:23:15 +0530

    modified port env variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const PORT =
-  process.env.NODE_ENV === "test" ? 3001 : process.env.NODE_APP_PORT || 3000;
+  process.env.NODE_ENV === "test" ? 3001 : process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
